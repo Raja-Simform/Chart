b6adb50dce2bdb460a00a7ff25d08172cc5f5cae
Raja-Simform <raja.jha@simformsolutions.com>
2025-06-19 16:18:26 +0530

    feat:add input feild

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,30 +1,78 @@
 import { InputText } from "primereact/inputtext";
 import styles from "./Input.module.css";
 import { Button } from "primereact/button";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useAppDispatch } from "../../store/ChartStore";
+import { addData } from "../../store/ChartSlice";
+
+import { Toast } from "primereact/toast";
+
 interface DataProps {
   stateX: string;
   stateY: string;
 }
 export default function Input() {
-  function handleSubmit(data: DataProps) {
-    console.log(data);
-  }
   const [stateX, setStateX] = useState<string>("");
   const [stateY, setStateY] = useState<string>("");
+  const toastTL = useRef<Toast>(null);
+  const dispatch = useAppDispatch();
+  function handleSubmit(data: DataProps) {
+    const dataX = data.stateX
+      .split(",")
+      .map((item) => item.trim())
+      .filter((item) => item !== "");
+    const dataY = data.stateY
+      .split(",")
+      .map((item) => item.trim())
+      .filter((item) => item !== "")
+      .map((item) => Number(item))
+      .filter((item) => !isNaN(item));
+
+    if (dataX.length === dataY.length && dataX.length !== 0) {
+      setStateX("");
+      setStateY("");
+      dispatch(
+        addData({
+          x_axis: dataX,
+          y_axis: dataY,
+        })
+      );
+      if (toastTL.current) {
+        toastTL.current.show({
+          severity: "success",
+          summary: "Success",
+          detail: "Chart data updated successfully!",
+          life: 500,
+        });
+      }
+    } else {
+      if (toastTL.current) {
+        toastTL.current.show({
+          severity: "warn",
+          summary: "Warn",
+          detail: "Add Valid Data!",
+          life: 500,
+        });
+      }
+    }
+  }
+
   return (
     <div className={styles.input}>
+      <p>Enter Input Data</p>
       <div className={styles.x_axis}>
         <p>X-axis Data (Labels)</p>
-        <h6>Enter comma-seperated values</h6>
-        <InputText onChange={(e) => setStateX(e.target.value)} />
+        <span>Enter comma-seperated values</span>
+        <InputText onChange={(e) => setStateX(e.target.value)} value={stateX} />
       </div>
 
       <div className={styles.y_axis}>
-        <h3>Y-axis Data (Values)</h3>
-        <h6>Enter comma-seperated values</h6>
-        <InputText onChange={(e) => setStateY(e.target.value)} />
+        <p>Y-axis Data (Values)</p>
+        <span>Enter comma-seperated values</span>
+        <InputText value={stateY} onChange={(e) => setStateY(e.target.value)} />
       </div>
+
+      <Toast ref={toastTL} position="top-left" />
       <Button
         type="button"
         label="Submit"
